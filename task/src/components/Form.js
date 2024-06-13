9fc08d348061f4561662768352154ca9cf67559a
adab786 <siddiquiadab@eng.rizvi.edu.in>
2024-06-14 00:30:33 +0530

    validation added

--- task/src/components/Form.js
+++ task/src/components/Form.js
@@ -3,6 +3,9 @@ import ReCAPTCHA from "react-google-recaptcha";
 
 const Form = () => {
   const [value, setvalue] = useState(null);
+  const haldkesubmit = () => {
+    return alert("submittded");
+  };
   return (
     <div className="m-5 flex ">
       <div className="text-3xl">
@@ -97,12 +100,11 @@ const Form = () => {
           <div class="relative z-0 w-full mb-5 group">
             <input
               type="tel"
-              pattern="[0-9]"
               name="floating_phone"
               id="floating_phone"
               class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               placeholder=" "
-              required
+              // required
             />
             <label
               for="floating_phone"
@@ -135,6 +137,7 @@ const Form = () => {
           </div>
         </div>
         <button
+          onClick={haldkesubmit}
           disabled={!value}
           type="submit"
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
